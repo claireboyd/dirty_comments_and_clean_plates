e16ab22efcfc475cc5c65b7a475398b509295cdf
claireboyd <claire.k.boyd@gmail.com>
2024-02-15 17:25:54 -0600

    finished pipeline to get restaurant data given points

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # how_the_bear_got_a_C
 Understanding how The Bear got a C: Using Restaurant Reviews to Predict Failed Restaurant Inspections in Chicago
+
+**Getting restaurant data**
+
+To replicate how we pulled in the restaurant data, you can use the below line of code:
+
+```bash
+poetry run python collect/get_restaurants.py --points_filepath collect/test_points.json --output_filename test_restuarant_pull
+```
+
+**Determining the points to pull data for**
+
+To replicate how we pulled random points to pull data from, 
+
--- collect/get_restaurants.py
+++ collect/get_restaurants.py
@@ -1,30 +1,18 @@
 from pathlib import Path
-import argparse
-import os, sys
-import pprint
-import requests
+import os, sys, json, requests, argparse
 from dotenv import load_dotenv
 
 #import vars from .env to os.environ
 load_dotenv()
 YELP_API_KEY = os.getenv('YELP_API_KEY')
 
-# example args
-lat = 41.791320
-long = -87.592710
-limit = 10
-
-url = f"https://api.yelp.com/v3/businesses/search?latitude={lat}&longitude={long}&sort_by=best_match&limit={limit}"
-
-
-def fetch_results(params, name):
+def fetch_results(params, limit=50):
     """
-    params (dict) includes valid params for a search_query
+    Uses a dictionary of params to send a search_query to the YELP Fusion API
     https://docs.developer.yelp.com/reference/v3_business_search
 
-    name to save the run as: 
+    Returns a requests response object.
     """
-
     try:
         #build query
         base_url="https://api.yelp.com/v3/businesses/search?"
@@ -33,40 +21,64 @@ def fetch_results(params, name):
         headers = {"accept": "application/json",
                    "Authorization": f"Bearer {YELP_API_KEY}"}
         
+        params["limit"] = limit
+
         #get response and save
         response = requests.get(url=base_url, params=params, headers=headers)
-
-
-        with open(f"data/restaurants_{name}.json", "w") as outfile:
-            outfile.write(response.json())
-        
+        return response        
 
     except Exception as e:
-        return str(e)
+        raise str(e)
 
-def main():
+def get_restaurants_for_points(points_filepath, output_filename):
+    """
+    Takes a filepath of a json object that has a key "points", with a value
+    that includes a list of dictionaries with keys for "latitude" and
+    "longitude" to get restaurant objects for.
 
-    params = {
-        "latitude": 41.791320,
-        "longitude": -87.592710,
-        "limit": 10
-    }
-
-    response = fetch_results(params, "test")
-
-    print(response)
-
-
+    Returns: nothing. Saves a json object in the data/ dir with the output 
+    filename that contains a dictionary with str key that concatenates
+    the lat/long points, and the value of a list of dictionaries that are all 
+    restaurant objects.
+    """
+    #initialize output dict/json object
+    output = {}
     
+    #load in params (list of points)
+    with open(points_filepath) as json_data:
+        points = json.load(json_data)
+
+    for point_params in points["points"]:
+        #create unique key for each lat/long combo
+        key = str(point_params['latitude'])+" , "+str(point_params['longitude'])
+        print(f"Starting with point: {key}")
+              
+        results = []
+        for i in range(0, 1000, 50):
+            print(f"Processing review {i}...")
+            point_params["offset"] = i
+
+            #get response for specific params
+            response = fetch_results(params=point_params)
+            results.append(response.json()['businesses'])
+
+        output[key] = [restaurant for batch in results for restaurant in batch]
+        print(f"Done with point: {key}")
+
+    with open(f"data/{output_filename}.json", "w") as outfile:
+        json.dump(output, outfile)
+
+def main(points_filepath: Path, output_filename: str):
+    get_restaurants_for_points(points_filepath, output_filename)
+
+
 def setup(args=None):    
-    parser = argparse.ArgumentParser(description='Gets JSON of Yelp Businesses from Lat/Long.')
+    parser = argparse.ArgumentParser(description='Gets JSON of Yelp Businesses from list of dictionaries of Lat/Long values.')
 
-    # ADD CLI ARGUMENTS HERE
-    #parser.add_argument('--latlong_file', required=True, type=Path, dest="latlong_file", help="Path to file with lat/longs.")
-
+    parser.add_argument('--points_filepath', required=True, type=Path, dest="points_filepath", help="Path to file with points for query.")
+    parser.add_argument('--output_filename', required=True, type=Path, dest="output_filename", help="File suffix for saving.")
     return parser.parse_args(args)
-    return
+
 
 if __name__ == "__main__":
-    #main(**vars(setup()))
-    response = main()
+    main(**vars(setup()))
